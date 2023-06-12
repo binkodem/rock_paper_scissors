93ffbe5061ab69824096e8bf60373fb2e01953ac
binkodem <nikodemann11@gmail.com>
2023-06-12 11:59:40 +0200

    Added UI

--- game.js
+++ game.js
@@ -1,42 +1,58 @@
-function game(){
-    let playerPoints=0
-    let computerPoints=0
+let playerPoints=0
+let computerPoints=0
 
-    for (let i = 0; i < 5; i++) {
-        function getComputerChoice(){
-            pick=["rock", "paper", "scissors"]
-            return pick[Math.floor(Math.random()*pick.length)]
-        }
-        
-        const playerSelection = prompt().toLowerCase();
-        const computerSelection = getComputerChoice();
-        
-        function playRound(playerSelection, computerSelection) {
-            if (playerSelection==computerSelection){
-                return "draw"
-            }
-            else if((playerSelection=="paper" && computerSelection=="rock") || (playerSelection=="scissors" && computerSelection=="paper") || (playerSelection=="rock" && computerSelection=="scissors")){
-                playerPoints+=1
-                return "player win"
+const buttons = document.querySelectorAll('button')
+    buttons.forEach((button) => {
+        button.addEventListener('click', () =>{
+            playerSelection=button.id
+            console.log(playerSelection)
+            const score = document.querySelector('#score')
+            if (playerPoints<5 && computerPoints<5){
+                game()
             }
             else{
-                computerPoints+=1
-                return "computer win" 
+                pass
             }
-            
-        }
-        console.log(playRound(playerSelection, computerSelection));
-    } 
+        })
+    })
 
-    if(playerPoints>computerPoints){
-        return "player win with "+playerPoints+" points"
-    }  
-    else if(playerPoints<computerPoints){
-        return "computer win with "+computerPoints+" points"
+function game(){
+    
+    const player = document.querySelector('#player')
+    const computer = document.querySelector('#computer')
+    
+
+    function getComputerChoice(){
+        pick=["rock", "paper", "scissors"]
+        return pick[Math.floor(Math.random()*pick.length)]
     }
-    else{
-        return "it's a draw"
+        
+    
+    const computerSelection = getComputerChoice();
+        
+    function playRound(playerSelection, computerSelection) {
+        if (playerSelection==computerSelection){
+            return "draw"
+        }
+        else if((playerSelection=="paper" && computerSelection=="rock") || (playerSelection=="scissors" && computerSelection=="paper") || (playerSelection=="rock" && computerSelection=="scissors")){
+            playerPoints+=1
+            return "player win"
+        }
+        else{
+            computerPoints+=1
+            return "computer win" 
+        }
+        
+    }
+    console.log(playRound(playerSelection, computerSelection));
+
+    player.textContent = "player has "+playerPoints+" points"
+    computer.textContent = "computer has "+computerPoints+" points"
+
+    if(playerPoints==5){
+        return score.textContent="player win"
+    }
+    else if(computerPoints==5){
+        return score.textContent="computer win"
     }
 }
-
-console.log(game())
